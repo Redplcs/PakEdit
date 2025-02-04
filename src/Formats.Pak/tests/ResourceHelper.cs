01d54d23b7e25debd26a4161abd79cc39eb5c486
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2025-02-04 21:10:47 +0300

    Add helper classes to get stream from resources

---
+++ src/Formats.Pak/tests/ResourceHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace CXModdingTools.PakEdit.Formats.Pak.Tests;
+
+internal static class ResourceHelper
+{
+	internal static Stream GetResourceStream(string resourceName)
+	{
+		var resourcePath = Path.Combine(Directory.GetCurrentDirectory(), "Resources", resourceName);
+
+		return File.OpenRead(resourcePath);
+	}
+}
---
+++ src/Formats.Pak/tests/Resources.cs
@@ -0,0 +1,6 @@
+﻿namespace CXModdingTools.PakEdit.Formats.Pak.Tests;
+
+internal static class Resources
+{
+	internal const string GlobalEK = @"GlobalEK.pak";
+}
