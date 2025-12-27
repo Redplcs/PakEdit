2495dfdbd6894c720f704692fef616ce6e6e5c4d
Redplcs <51379496+Redplcs@users.noreply.github.com>
2025-12-28 04:57:24 +0500

    Initial commit

---
+++ src/PakEdit/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Redplcs.PakEdit;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ src/PakEdit/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Redplcs.PakEdit;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
